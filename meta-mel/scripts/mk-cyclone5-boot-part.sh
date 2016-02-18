962df71fa5933d8a09b19e2f0e75dd637c68bd74
Fahad Usman <fahad_usman@mentor.com>
2016-02-18 17:27:00 +0500

    Cyclone5: Add script to create boot partition
    The SD card for Cyclone-5 needs a custom partition with type=0xa2. It is
    required by the BootROM to read the boot loader binary. Currently wic does
    not have support for assigning an arbitrary value to partition id. This
    script create the special partition using fdisk utility. The SD-Card for
    Cyclone 5 must be created using wic image before using this script.
    
    Signed-off-by: Fahad Usman <fahad_usman@mentor.com>

---
+++ meta-mel/scripts/mk-cyclone5-boot-part.sh
@@ -0,0 +1,99 @@
+#! /bin/sh
+# Script to create special partition for bootloader of Altera Cyclone-5.
+#
+# Author: Fahad Usman, Mentor Graphics Inc.
+
+# Licensed under terms of GPLv2
+#
+
+VERSION="0.1"
+
+SELF=$(basename $0)
+SELFPATH=$(dirname $0)
+
+binary="./u-boot-with-spl-dtb.sfp"
+
+BOOT_PT_NUM="3"
+BOOT_PT_TYPE="a2"
+BOOT_PT_START="2048"
+BOOT_PT_END="32775"
+
+P="p"
+
+# only Superuser can run this script
+if [ ${UID} -ne 0 ] ; then
+    echo "Please run this script as a Superuser."
+    exit -1
+fi
+
+usage ()
+{
+  echo "
+Usage: `basename $0` -d device [-f binary]
+"
+  exit 1
+}
+
+# Process command line...
+while [ $# -gt 0 ]; do
+  case $1 in
+    --help | -h)
+      usage $0
+      ;;
+    -d) shift; device=$1; shift; ;;
+    -f) shift; binary=$1; shift; ;;
+    *) copy="$copy $1"; shift; ;;
+  esac
+done
+
+test -z $device && usage $0
+
+if [ ! -b $device ]; then
+   echo "ERROR: $device is not a block device file"
+   exit 1;
+fi
+
+if [ -b ${device}1 ]; then
+  P=""
+else
+  P="p"
+fi
+
+if [ ! -f ${binary} ]; then
+  echo "ERROR: failed to find the boot loader image file: \"${binary}\""
+  exit 1;
+fi
+
+echo "*****************************************************************"
+echo "*         This script is specifically written to be used        *"
+echo "*         with SD-Card prepared with wic image generated        *"
+echo "*         by building MEL for Altera Cyclone V target.          *"
+echo "*                                                               *"
+echo "*         Make sure you have prepared the SD-Card following     *" 
+echo "*         the instructions in the MEL getting started guide     *"
+echo "*         for Altera Cyclone V.                                 *"
+echo "*                                                               *"
+echo "*         This script alters the partition table of SD-Card     *"
+echo "*         and the changes made by this script may not be        *"
+echo "*         reversible                                            *"
+echo "*                                                               *"
+echo "*         Press <ENTER> to confirm....                          *"
+echo "*****************************************************************"
+read junk
+
+cat <<END | fdisk ${device} >/dev/null 2>&1
+n
+p
+${BOOT_PT_NUM}
+${BOOT_PT_START}
+${BOOT_PT_END}
+t
+${BOOT_PT_NUM}
+${BOOT_PT_TYPE}
+w
+END
+
+dd if=${binary} of=${device}${P}${BOOT_PT_NUM} 2>/dev/null
+
+echo "completed!"
+exit 0
